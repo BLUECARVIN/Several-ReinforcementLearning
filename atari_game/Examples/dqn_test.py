76746b597959948acce5598e01aba79ed79724bc
bluecarvin <bluecarvin@163.com>
2020-07-21 14:26:05 +0800

    update

--- atari_game/Examples/dqn_test.py
+++ atari_game/Examples/dqn_test.py
@@ -5,8 +5,8 @@ import Agent
 import gym
 import numpy as np
 
-env = gym.make('Asteroids-v0')
+env = gym.make('Pong-v0')
 # a = np.empty([1000000, 84, 84], dtype=np.uint8)
 # agent = Agent.DoubleDQNAgent(env, 1626, '/home/szchen/Several-ReinforcentLearning/atari_game/Examples/Log/')
-agent = Agent.DoubleDQNAgent(env, 12, './Asteroids', learning_start=10000, memory_size=1000000)
+agent = Agent.DoubleDQNAgent(env, 12, './Breakout', learning_start=10000, memory_size=1000000)
 agent.train(is_render=True)
--- atari_game/Examples/test.py
+++ atari_game/Examples/test.py
@@ -5,6 +5,6 @@ import Agent
 import gym 
 import numpy as np
 
-env = gym.make('Asteroids-v0')
+env = gym.make('Breakout-v0')
 agent = Agent.DoubleDQNAgent(env, 12, None, memory_size=1000)
 agent.test(is_render=True, path='./Log2/DQNtest.pt', epoch=100)
